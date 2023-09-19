d28e208f2805e9d63d5a73904d3d4f397d5699a0
Kle012 <kimhle.0102@gmail.com>
2023-09-18 22:58:32 -0700

    database setup

---
+++ db.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/** Database setup for pokemon. */
+
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+let db;
+
+if (process.env.NODE_ENV === "test") {
+    db = new Client({
+        connectionString: getDatabaseUri(),
+        // ssl: {
+        //     rejectUnauthorized: false
+        // }
+    });
+} else {
+    db = new Client({
+        connectionString: getDatabaseUri()
+    });
+}
+
+db.connect();
+
+module.exports = db;
